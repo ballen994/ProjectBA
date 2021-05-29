c8af5df62b71dfc8ec9232d09321b037389e1f7c
Brian <brian.allen1@hotmail.com>
2021-05-29 15:03:50 +0100

    Progress to 29/05/2021

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 # Import CSV
 import pandas as pd
+from datetime import timedelta
+import numpy as np
+import seaborn as sns
 avocado_data = pd.read_csv("avocado.csv")
 print(avocado_data.info)
 print(avocado_data.dtypes)
 avocado_data.Date = pd.to_datetime(avocado_data.Date)
-Calavo_SP= pd.read_csv("CVGW.csv")
+Calavo_SP = pd.read_csv("CVGW.csv")
 print(Calavo_SP.info)
 print(Calavo_SP.shape)
 print(Calavo_SP.dtypes)
 Calavo_SP.Date = pd.to_datetime(Calavo_SP.Date)
-# import modules
-
-from datetime import timedelta
+# Merge  on date
 # Ensure date datatype on Columns
-avocado_data['Date']  = pd.to_datetime(avocado_data['Date'],format='%Y-%m-%d')
-Calavo_SP['Date']  = pd.to_datetime(Calavo_SP['Date'],format='%Y-%m-%d')
-
+avocado_data['Date'] = pd.to_datetime(avocado_data['Date'], format='%Y-%m-%d')
+Calavo_SP['Date'] = pd.to_datetime(Calavo_SP['Date'], format='%Y-%m-%d')
 #Merge Cavalo_SP and avocado_data on Day -1
-
 # Create column with new date
 Calavo_SP['date_minus_one'] = Calavo_SP.Date - timedelta(days=1)
-
-avo_and_share = Calavo_SP.merge(avocado_data,how='inner', left_on='date_minus_one',right_on='Date')
-
+avo_and_share = Calavo_SP.merge(avocado_data, how='inner', left_on='date_minus_one', right_on='Date')
 print(avo_and_share.info)
 print(avo_and_share.shape)
-
 # API for Calavo Growers
 import matplotlib.pyplot as plt
 from alpha_vantage.timeseries import TimeSeries
@@ -47,32 +43,100 @@ plt.plot(stock_data['close'], lw=.5, linestyle='-')
 plt.xlabel('Time and Date', fontsize=15, fontweight='bold')
 plt.ylabel('$USD', fontsize=15, fontweight='bold')
 plt.title('Stock Price over last 100 minutes', fontsize=25, fontweight='bold')
+plt.plot()
 
-#Sorting, Indexing and Grouping
+# How Many years of Avocado Data in DataFrame
+print(avocado_data.Date.dt.strftime('%Y').unique())
+# How Many Different Regions are in the Avocado DataFrame?
+print('# of Regions:', avocado_data.region.nunique())
+# How Many Different Regions are in the Avocado DataFrame?
+print('# of Types:', avocado_data.type.unique())
+# How Does the Average Price of an Avocado Per Region Compare
+average_prices = avocado_data.groupby(['region', 'type']).AveragePrice.mean().reset_index(name='price')
+# What is the cheapest and most expensive region (on average over all years) for both Conventional and Organic Avocados
+print('\n')
+print('Cheapest Region For Avocados')
+for avo_type in average_prices.type.unique():
+    lowest_price = average_prices[average_prices.type == avo_type].price.min()
+    print(avo_type, ':', average_prices[(average_prices.price == lowest_price) &
+                                      (average_prices.type == avo_type)
+                                             ].region.to_string(index=False))
+print('\n')
+print('Most Expensive Region For Avocados')
 
-##Step 1 - Set index as date
-avocado_data_index = avocado_data.set_index('Date')
+for avo_type in average_prices.type.unique():
+    highest_price = average_prices[average_prices.type == avo_type].price.max()
+    print(avo_type, ':', average_prices[(average_prices.price == highest_price) &
+                                      (average_prices.type == avo_type)
+                                             ].region.to_string(index=False))
 
-##Step 2 - Sort by Region
-avocados_by_region = avocado_data_index.sort_values('region', ascending=True)
+# How does the price for All US of Convential Avocados compare to Organice
+data = avocado_data[avocado_data.region == 'TotalUS'].groupby(['year', 'type']).AveragePrice.mean().reset_index(
+    name='price')
+data.year = pd.to_datetime(data.year, format='%Y')
 
-###Step3 - Use Groupby and sum to calculate sales by region
-sales_by_region = avocados_by_region.groupby('region')['Total Bags'].sum()
-print(sales_by_region)
+data.pivot_table(index='year',columns='type').plot(figsize=(10, 10/1.6))
+plt.title('Average Price for all US of Avocados')
+plt.ylabel('Price [$]')
+plt.xlabel('Year')
 
+# Avocado Prices for the Top 5 Regions Based on Total Volume
 
-#Identify missing Values/Dropping duplicates
-
-##Make Nil values for average columns as the one before/after
-
-### Looping/ Iterrows
-
-
-#### Merge a historical price data and avocado data
-
-
-
+region_totals = avocado_data.groupby('region')['Total Volume'].sum()
+top5_regions = np.array(region_totals[region_totals.index != 'TotalUS'].sort_values(ascending=False).head(5).index)
 
 
+plt.figure(figsize=(10,10/1.6))
+for region in top5_regions:
+    plt.plot(avocado_data[(avocado_data.region == region) &
+                         (avocado_data.type == 'conventional')].Date
+             ,avocado_data[(avocado_data.region == region) &
+                         (avocado_data.type == 'conventional')].AveragePrice,label=region)
+plt.legend()
+plt.title('Price of Avocado - Top 5 Regions by Total Volume')
+plt.ylabel('Price [$]')
+plt.show()
 
+# How does the share price of Calavo Correlate with US Price of Avocado
 
+# Make sure the dataframe is sorted by date
+avo_and_share.sort_values('Date_x', ascending=True)
+
+fig, ax1 = plt.subplots()
+fig.set_size_inches(8, 8/1, 6)
+
+colors = ['r','k']
+# Plot On Same Graph with Different Axis
+for i, avo_type in enumerate(avo_and_share.type.unique()):
+
+    ax1.plot(avo_and_share[(avo_and_share.region == 'TotalUS') &
+                 (avo_and_share.type == avo_type)].Date_x,
+             avo_and_share[(avo_and_share.region == 'TotalUS') &
+                (avo_and_share.type == avo_type)].AveragePrice
+                         , colors[i]
+             , label='Average Price of {} Avocado'.format(region))
+    plt.legend()
+
+ax1.set_ylabel('Average Price of Avocado')
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+ax2.plot(avo_and_share[(avo_and_share.region == 'TotalUS') &
+                       (avo_and_share.type == avo_type)].Date_x, avo_and_share
+                        [(avo_and_share.region == 'TotalUS') & (avo_and_share.type == avo_type)].Open,
+                          label='Opening Share Price', color='g')
+ax2.set_ylabel('Calavo Stock Price')
+plt.legend()
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.show()
+
+# Create a DataFrame with Avocado Prices and Stock Price to find correlation
+
+data = avo_and_share[avo_and_share.region == 'TotalUS'].pivot_table(index='Date_x',columns='type',values='AveragePrice').reset_index()
+data = data.merge(avo_and_share.groupby(['Date_x', 'Open']).size().reset_index().iloc[:, :2], on='Date_x')
+data.set_index('Date_x', inplace=True)
+
+f, ax = plt.subplots(figsize=(10, 10/1.6))
+sns.heatmap(np.round(data.corr(), 4), annot=True, linewidths=.5, ax=ax)
+plt.ylim(0,3)
+plt.tight_layout()
+plt.show()
